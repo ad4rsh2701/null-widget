b09734d6e005c61193c571b80087666523138d0d
adarsh2701 <adarsharyan2701@gmail.com>
2026-03-30 11:15:53 +0530

    refactor(Fonts.hpp): Add `LoadCustomFonts()` and light docs - Also `FW_NORMAL` is passed as default param for weight

--- include/Fonts.hpp
+++ include/Fonts.hpp
@@ -3,7 +3,18 @@
 #include <windows.h>
 
 namespace WidgetFonts {
-    HFONT Urbanist(int size, int weight);
-    HFONT Doto(int size, int weight);
-    HFONT KodeMono(int size, int weight);
+
+    /*
+     * @brief Loads Custom Fonts to GDI memory
+     */
+    void LoadCustomFonts();
+
+    // Create the URBANIST font
+    HFONT Urbanist(int size, int weight = FW_NORMAL);
+
+    // Create the DOTO font
+    HFONT Doto(int size, int weight = FW_NORMAL);
+
+    // Create the KODE MONO font
+    HFONT KodeMono(int size, int weight = FW_NORMAL);
 }
